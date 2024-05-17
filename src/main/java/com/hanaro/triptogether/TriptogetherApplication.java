b2ff5bab084a6c3a93b5152d8aa9308a01fe1d08
mummhy0811 <mummhy0811@gmail.com>
2024-05-17 14:35:40 +0900

    init

---
+++ src/main/java/com/hanaro/triptogether/TriptogetherApplication.java
@@ -0,0 +1,13 @@
+package com.hanaro.triptogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptogetherApplication.class, args);
+	}
+
+}
