fcb9110f8b881dd86b892a795fce41f2d633a8b9
yubin-im <ybyb130@gmail.com>
2024-05-20 16:24:26 +0900

    rename: 모임서비스 전체 조회 메소드 이름 변경

--- src/main/java/com/hanaro/triptogether/account/controller/AccountController.java
+++ src/main/java/com/hanaro/triptogether/account/controller/AccountController.java
@@ -1,6 +1,6 @@
 package com.hanaro.triptogether.account.controller;
 
-import com.hanaro.triptogether.account.dto.response.AccountListResDto;
+import com.hanaro.triptogether.account.dto.response.TeamServiceListResDto;
 import com.hanaro.triptogether.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +17,10 @@ public class AccountController {
 
     // 모임서비스 전체 조회
     @PostMapping("/account")
-    public List<AccountListResDto> accountList(@RequestBody Map<String, Long> memberIdxMap) {
+    public List<TeamServiceListResDto> accountList(@RequestBody Map<String, Long> memberIdxMap) {
         Long memberIdx = memberIdxMap.get("memberIdx");
-        List<AccountListResDto> accountListResDtos = accountService.accountList(memberIdx);
+        List<TeamServiceListResDto> teamServiceListResDtos = accountService.TeamServiceList(memberIdx);
 
-        return accountListResDtos;
+        return teamServiceListResDtos;
     }
 }
--- src/main/java/com/hanaro/triptogether/account/dto/response/TeamServiceListResDto.java
+++ src/main/java/com/hanaro/triptogether/account/dto/response/TeamServiceListResDto.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Builder
-public class AccountListResDto {
+public class TeamServiceListResDto {
     private Long accIdx;
     private String accNumber;
     private BigDecimal accBalance;
