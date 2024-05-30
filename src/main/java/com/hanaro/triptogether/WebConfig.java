768314caded46119cf3d06b77b80dbdfa465fe94
mummhy0811 <mummhy0811@gmail.com>
2024-05-30 17:21:28 +0900

    setting: cors수정

--- src/main/java/com/hanaro/triptogether/WebConfig.java
+++ src/main/java/com/hanaro/triptogether/WebConfig.java
@@ -14,7 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .addMapping("/**")
                 .allowedHeaders("*")
                 .allowedMethods("*")
-                .allowedOrigins("*")
-                .allowCredentials(true);
+                .allowedOrigins("*");
     }
 }
