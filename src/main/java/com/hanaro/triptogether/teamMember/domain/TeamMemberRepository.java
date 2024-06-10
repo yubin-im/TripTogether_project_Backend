0b11286814198de9091c70578a2aa642dd46b979
yubin-im <ybyb130@gmail.com>
2024-06-10 11:11:17 +0900

    fix: 모임가입- 기존 멤버 대응 기능 추가

--- src/main/java/com/hanaro/triptogether/teamMember/domain/TeamMemberRepository.java
+++ src/main/java/com/hanaro/triptogether/teamMember/domain/TeamMemberRepository.java
@@ -1,5 +1,6 @@
 package com.hanaro.triptogether.teamMember.domain;
 
+import com.hanaro.triptogether.member.domain.Member;
 import com.hanaro.triptogether.team.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -18,4 +19,6 @@ public interface TeamMemberRepository extends JpaRepository<TeamMember,Long> {
     List<TeamMember> findTeamMemberByMember_MemberIdx(Long memberIdx);
 
     Optional<TeamMember> findTeamMemberByMember_MemberIdxAndTeam_TeamIdx(Long memberIdx, Long teamIdx);
+
+    TeamMember findTeamMemberByMemberAndTeam(Member member, Team team);
 }
